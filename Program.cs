c4995775e7be617229ba1c1ade17759f3550f26f
Amazing Cabiles <amazinggracecabiles@gmail.com>
2023-11-10 17:10:59 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Final_Project_ITEC_102
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Made by Amazing Cabiles, Cyrelle Gapit, Francen Manalo from BSCS-1B
+
+            // Title Screen 
+
+            // Main Code
+
+            // Character Movement
+        }
+    }
+}
